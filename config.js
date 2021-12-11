2bb48867d5e61ca053bbafda60910fb73d1b2c83
AryanMithari <78090185+AryanMithari@users.noreply.github.com>
2021-12-11 19:34:18 +0530

    Update config.js
    Replaced the SDK with my config.js

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-import firebase from 'firebase';
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyDCncCBQuVbhwz3nWx4a2LtL2rmhkEHcYY",
+    authDomain: "project-67-8e4b8.firebaseapp.com",
+    projectId: "project-67-8e4b8",
+    storageBucket: "project-67-8e4b8.appspot.com",
+    messagingSenderId: "336890634655",
+    appId: "1:336890634655:web:68f375698a98c7355e9daa"
+  };
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default firebase.database();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
